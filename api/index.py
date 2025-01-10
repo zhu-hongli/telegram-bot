1a0888e631fbcb3cc736709ec98e326d0efb0f7b
zhu-hongli <zhl19960510@163.com>
2025-01-10 20:17:07 +0800

    feat(): zengjia bot

--- api/index.py
+++ api/index.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI, Request
-# from telegram import Update, Bot
-# from telegram.ext import Dispatcher, CommandHandler
+from telegram import Update, Bot
+from telegram.ext import Dispatcher, CommandHandler
 
 app = FastAPI()
 
-# TOKEN = '7015054463:AAHRjapJy3Rkbz3JTC_IjsjhklrzO1XBhb0'
-# bot = Bot(token=TOKEN)
-# dispatcher = Dispatcher(bot, None, workers=0)
+TOKEN = '7015054463:AAHRjapJy3Rkbz3JTC_IjsjhklrzO1XBhb0'
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher(bot, None, workers=0)
 
-# def start(update, context):
-#     update.message.reply_text('Hello! This is a webhook demo bot.')
+def start(update, context):
+    update.message.reply_text('Hello! This is a webhook demo bot.')
 
-# dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('start', start))
 
-# @app.post("/webhook")
-# async def webhook(request: Request):
-#     json_data = await request.json()
-#     update = Update.de_json(json_data, bot)
-#     dispatcher.process_update(update) 
-#     return "ok"
+@app.post("/webhook")
+async def webhook(request: Request):
+    json_data = await request.json()
+    update = Update.de_json(json_data, bot)
+    dispatcher.process_update(update) 
+    return "ok"
 
 @app.get("/")
 async def index():
